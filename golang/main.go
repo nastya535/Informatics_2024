10aeac3f0afc753e7aa6627562367c7500ff3d09
nastya533 <mxswsx@mail.ru>
2024-10-30 08:35:25 +0300

    Murashova_laba4

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	laba4 "isuct.ru/informatics2022/labs/laba4"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("Мурашова Анастасия Витальевна")
+	laba4.Laba4run()
 }
