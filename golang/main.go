960477c8eeb6c7ab464e2705af728214fa2aee10
nastya533 <mxswsx@mail.ru>
2024-11-02 22:17:01 +0300

    laba6

--- golang/main.go
+++ golang/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 
+	lab6 "isuct.ru/informatics2022/laba6"
 	lab4 "isuct.ru/informatics2022/labs"
 )
 
 func main() {
 	fmt.Println("Мурашова Анастасия Витальевна")
 	lab4.Laba4run()
+	lab6.Laba6Run()
 }
