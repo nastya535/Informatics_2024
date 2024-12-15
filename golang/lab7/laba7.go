5e6fe3569a03294a9bdaa850167e0a41f62fd8dd
nastya533 <mxswsx@mail.ru>
2024-12-15 21:09:59 +0300

    Murashova_laba7

---
+++ golang/lab7/laba7.go
@@ -0,0 +1,31 @@
+package lab7
+
+import (
+	"fmt"
+)
+
+func CalculationSumProduct(listProducts []Product) string {
+	var sum float64 = 0
+	for _, product := range listProducts {
+		sum += product.GetPrice()
+	}
+	s := fmt.Sprintf("%.2f", sum)
+	return s
+}
+
+func Lab7Run() {
+	lipstick := &Cosmetic{name: "помада", price: 400, brand: "influence"}
+	ship := &Ship{name: "яхта", material: "алюминий", price: 9000000}
+	doll := &Toy{name: "барби", price: 1500, material: "пластик"}
+
+	doll.SetPrice(3000)
+	ship.SetMaterial("сталь")
+	lipstick.SetBrand("NYX")
+
+	listProducts := []Product{lipstick, ship, doll}
+	fmt.Printf("Сумма товаров, без учета скидки, равна: %v рублей \n", CalculationSumProduct(listProducts))
+	lipstick.ApplyDiscount(5)
+	ship.ApplyDiscount(15)
+	doll.ApplyDiscount(10)
+	fmt.Printf("Сумма товаров, без учета скидки, равна: %v рублей \n", CalculationSumProduct(listProducts))
+}
